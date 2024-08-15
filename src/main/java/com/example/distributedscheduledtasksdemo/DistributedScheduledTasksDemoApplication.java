8f32ceab03bbb31aeb12dc68413e0ae6571f1394
jiarong <sxyang@iflytek.com>
2024-08-15 17:30:12 +0800

    分布式定时任务

---
+++ src/main/java/com/example/distributedscheduledtasksdemo/DistributedScheduledTasksDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.distributedscheduledtasksdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedScheduledTasksDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedScheduledTasksDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/distributedscheduledtasksdemo/DistributedScheduledTasksDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.distributedscheduledtasksdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedScheduledTasksDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
